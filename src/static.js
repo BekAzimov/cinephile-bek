a2a2005295a0f55959398ff2404f9146be47bdd5
Bek <3209094@gmail.com>
2023-11-21 21:04:03 +0500

    first commit

---
+++ src/static.js
@@ -0,0 +1,10 @@
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNTMxZDQxN2Q2YTNiOWIzY2NhNTU5ZDljMmExNzc4ZiIsInN1YiI6IjY1MDliNjZhODI2MWVlMDBhY2ZlOWI2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ypKO_UzL9qqIlHrsQH9xpJd7quQoeYsLYWA8ZbKZqsc'
+    }
+  };
+  
+  export const imgUrl = 'https://image.tmdb.org/t/p/w500';
+  export const imgUrlFull = 'https://image.tmdb.org/t/p/original';
